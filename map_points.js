b4319cc4950c3d44152f45093cc0d48dfa5da63a
QuestwalkerKO <94035647+Questwalker@users.noreply.github.com>
2024-11-21 11:45:17 -0700

    in development code

--- map_points.js
+++ map_points.js
@@ -1,9 +1,10 @@
-const map_size = 896;
-var references = {}
+var points_section = document.getElementById('information_points')
+var references = {'icons': {}}
 // var references = {
-//     "category_satellite_dishes": {
-//         "categoryhead": Object,
-//         "entrycontainer": Object,
+//     "category__satellite_dishes": {
+//         "menuparent": Object, // The base container that holds both the parent and the children element (has class .category_parent)
+//         "menuchildren": Object, // The container that holds all the children (has class .category_children)
+//         "leafletgroup": Object, // The layergroup that contains all leaflet markers in the category 
 //         1: {
 //             "point": Object,
 //             "entry": Object
@@ -12,190 +13,148 @@ var references = {}
 //             "point": Object,
 //             "entry": Object
 //         }
+//     },
+//     'icons': {
+//         'burger': `<leaflet icon reference>`
 //     }
 // }
 
-function hideEntries(element) {
-    let entrycontainer = references[element.parentElement.parentElement.id]['entrycontainer']
-    if (entrycontainer.classList.contains('hidden')) {
-        // unhide
-        entrycontainer.classList.remove('hidden')
-        element.innerHTML = '&nbsp;▼'
-    } else {
-        // hide
-        entrycontainer.classList.add('hidden')
-        element.innerHTML = '&nbsp;-'
-    }
+function convertUnrealToGame([x,y]) {return [x / 100, y / 100]}
+function convertGameToUnreal([x,y]) {return [x * 100, y * 100]}
+function convertGameToLeaflet([x,y]) {return [-y + (mapSize/2), x + (mapSize/2)]}
+function convertLeafletToGame([y,x]) {return [x - mapSize/2, -(y - mapSize/2)]}
+function convertUnrealToLeaflet([x,y]) {return convertGameToLeaflet(convertUnrealToGame([x,y]))}
+function convertLeafletToUnreal([y,x]) {return convertGameToUnreal(convertLeafletToGame([y,x]))}
+function roundNumber(number, digit=0) {return Math.round((number * Math.pow(10, digit)) * (1 + Number.EPSILON)) / Math.pow(10, digit)}
+
+function pointClickEvent(e) {
+    console.log(this.options.pointindex)
 }
 
-function hideCategory(element) {
-    let id = element.parentElement.parentElement.id
-    let force
-    if (element.innerText == '[H]') {
-        // unhide points
-        element.innerText = '[S]'
-        element.classList.remove('gray')
-        force = 'show'
-    } else {
-        // hide points
-        element.innerText = '[H]'
-        element.classList.add('gray')
-        force = 'hide'
-    }
-    Object.keys(references[id]).forEach(key => {
-        if (!isNaN(key) && !isNaN(parseFloat(key))) {
-            // it is a number
-            // TODO: Make this IF statement unnessessary by spliting the pointindexes into a subcategory of references
-            hidePoint(id, key, force=force)
-        }
-    });
-}
-
-function hidePointCommand(element) {
-    hidePoint(element.parentElement.parentElement.parentElement.id, element.dataset.pointindex)
-}
-
-function hidePoint(id, pointindex, force=undefined) {
-    let point = references[id][pointindex]['point']
-    let button = references[id][pointindex]['entry'].querySelector('[data-pointindex]')
-    if ((point.classList.contains('hidden') || force == 'show') && (force != 'hide')) {
-        // unhide point
-        button.innerText = '[S]'
-        button.classList.remove('gray')
-        point.classList.remove('hidden')
-    } else {
-        // hide point
-        button.innerText = '[H]'
-        button.classList.add('gray')
-        point.classList.add('hidden')
-    }
+function toggleCategoryButton(e) {
+    console.log(references[this.parentElement.id])
 }
 
 
 
-// creating the points on the map
+
+
+
 points.forEach((data, pointindex) => {
-    // creates the category tag that the point will be sorted into
     let categoryname = `category_${(data.category != '' && data.category != undefined) ? data.category.toLowerCase().replaceAll(' ','_') : 'miscellaneous'}`
 
-    // Create element. Add classes, add positioning, add pointindex data, prevent dragging, add icon, and add the element to the website
-    let pointelement = document.createElement('img');
-    pointelement.classList.add('image_label')
-    pointelement.classList.add(data.positioning)
-    pointelement.style.left = ((((data.xPos + 745) / 1490) * map_size) + 896).toString()+'px';
-    pointelement.style.top = ((((data.yPos + 745) / 1490) * map_size) + 896).toString()+'px';
-    pointelement.dataset.pointindex = pointindex
-    pointelement.ondragstart = () => { return false; }
-    pointelement.src = data.icon
-    map_container.appendChild(pointelement)
-
-
-
-
-
-    // generate entries for points tab
-    //  category detection
-    if (document.getElementById(categoryname) == null) { // if category entry does not exist yet..
-        // create containing category element
-        let parentelement = document.createElement('div')
-        parentelement.id = categoryname
-
-        //
-        let headelement = document.createElement('div')
-        headelement.classList.add('pointlist_category')
-
-        // hide / show button
-        let headelement_visiblitybutton = document.createElement('div')
-        headelement_visiblitybutton.classList.add('showhidebutton')
-        headelement_visiblitybutton.classList.add('pointercursor')
-        headelement_visiblitybutton.classList.add('unselectable')
-        headelement_visiblitybutton.setAttribute('onclick', "hideCategory(this)")
-        headelement_visiblitybutton.innerText = '[S]'
-        headelement.appendChild(headelement_visiblitybutton)
-
-        // text label
-        let headelement_label = document.createElement('div')
-        headelement_label.classList.add('pointlistentry_name')
-        headelement_label.innerText = (data.category != '' && data.category != undefined) ? data.category : 'Miscellaneous'
-        headelement.appendChild(headelement_label)
-
-        // collapse / expand button
-        let headelement_collapse = document.createElement('div')
-        headelement_collapse.classList.add('pointercursor')
-        headelement_collapse.classList.add('unselectable')
-        headelement_collapse.setAttribute('onclick', "hideEntries(this)")
-        headelement_collapse.innerHTML = '&nbsp;-' // collapsed by default #1/2
-        headelement.appendChild(headelement_collapse)
-
-        parentelement.appendChild(headelement)
-        information_pane_points.appendChild(parentelement)
-        
-        // create container for the entries
-        let entrycontainer = document.createElement('div')
-        entrycontainer.classList.add('hidden') // collapsed by default #2/2
-        parentelement.appendChild(entrycontainer)
-
-        // store reference
-        references[categoryname] = {'categoryhead': headelement, 'entrycontainer': entrycontainer}
+    // If the icon hasn't been loaded before, load it into ref.icons
+    if (!references.icons[data.icon]) {
+        references.icons[data.icon] = L.icon({
+            iconUrl: data.icon,
+            iconSize: [24, 24],
+        });
     }
+    // If the category hasn't been loaded before, create it and add some basic data
+    if (!references[categoryname]) {
+        console.log(categoryname)
+        
+        // <div id="category__transformers" class="category_parent">
+        //     <div class="category_parentbutton">[S]</div>
+        //     <div class="category_parentname">Satellite Dishes</div>
+        // </div>
 
-    //  add point to respective category
-    let entryelement = document.createElement('div')
-    entryelement.classList.add('pointlist_entry')
+        // Create generic category container element
+        let categorycontainer = document.createElement('div')
+        categorycontainer.id = categoryname
+        categorycontainer.classList.add('category_parent')
+        points_section.appendChild(categorycontainer)
 
-    // hide / show button
-    let entryelement_visiblitybutton = document.createElement('div')
-    entryelement_visiblitybutton.classList.add('pointercursor')
-    entryelement_visiblitybutton.classList.add('unselectable')
-    entryelement_visiblitybutton.setAttribute('onclick', "hidePointCommand(this)")
-    entryelement_visiblitybutton.dataset.pointindex = pointindex
-    entryelement_visiblitybutton.innerText = '[S]'
-    entryelement.appendChild(entryelement_visiblitybutton)
+        // Show/Hide button
+        let visibilitybutton = document.createElement('div')
+        visibilitybutton.classList.add('category_parentbutton')
+        visibilitybutton.classList.add('unselectable')
+        visibilitybutton.innerText = '[S]'
+        visibilitybutton.addEventListener('click', toggleCategoryButton)
+        categorycontainer.appendChild(visibilitybutton)
 
-    // text label
-    let entryelement_label = document.createElement('div')
-    entryelement_label.classList.add('pointlistentry_name')
-    entryelement_label.innerText = data['name']
-    entryelement.appendChild(entryelement_label)
+        // Text label
+        let textlabel = document.createElement('div')
+        textlabel.classList.add('category_parentname')
+        textlabel.innerText = (data.category != '' && data.category != undefined) ? data.category : 'Miscellaneous'
+        categorycontainer.appendChild(textlabel)
 
-    // store reference, add child
-    references[categoryname][pointindex] = {'entry': entryelement, 'point': pointelement}
-    references[categoryname]['entrycontainer'].appendChild(entryelement)
+        // add references
+        references[categoryname] = {
+            'menuparent': null,
+            'menuchildren': null,
+            'leafletgroup': L.layerGroup()
+        }
+        layercontrol.addOverlay(references[categoryname].leafletgroup, categoryname) //DEBUG
+    }
+    // Create the marker
+    let marker = L.marker(
+        convertGameToLeaflet([data.xPos,data.yPos]), 
+        {
+            icon: references.icons[data.icon],
+            'pointindex': pointindex
+        }
+    )
+    // Bind click event, add popup, add to category layer
+    marker.on('click', pointClickEvent)
+    marker.bindPopup(L.popup({
+        'content': data.name,
+        'offset': [0,3]
+    }))
+    marker.addTo(references[categoryname].leafletgroup)
 
 
 
 
 
-    
+
+
+
+
 })
+
 
 lines.forEach((data, lineindex) => {
-    ctx.beginPath();
-    data['coordinates'].forEach((coords, index) => {
-        if (index == 0) {
-            ctx.moveTo(((coords[0]+745)/1490)*map_size+896, ((coords[1]+745)/1490)*map_size+896);
-        } else {
-            ctx.lineTo(((coords[0]+745)/1490)*map_size+896, ((coords[1]+745)/1490)*map_size+896);
+    let polygon = L.polyline(data.coordinates.map(convertGameToLeaflet),
+        {
+            'smoothFactor': 0.5,
+            'color': data.color,
+            'weight': data.linethickness,
+            'lineCap': 'square',
+            'lineJoin': 'miter',
+            'fill': data.fill,
+            'fillColor': data.fill,
+            'interactive': false
         }
-    })
-    if (!(data['fill'] == undefined)) {
-        ctx.closePath();
-        ctx.fillStyle = data['fill'];
-        ctx.fill();
+    )
+    if (data.category) {
+        // limitation: the line can only add itself to layers that already exist, otherwise it will error
+        polygon.addTo(references[`category_${(data.category != '' && data.category != undefined) ? data.category.toLowerCase().replaceAll(' ','_') : 'miscellaneous'}`].leafletgroup)
+    } else {
+        polygon.addTo(map)
     }
-    ctx.lineWidth = data['linethickness'];
-    ctx.strokeStyle = data['color'];
-    ctx.lineCap = "square";
-    ctx.lineJoin = "miter";
-    ctx.stroke();
 })
 
 
 
 
 
-// hide categories by default
-// TODO: make a better way of doing this
-hideCategory(references.category_chicken_burgers.categoryhead.querySelector('.showhidebutton'))
-hideCategory(references.category_halloween_pumpkins.categoryhead.querySelector('.showhidebutton'))
-// console.log(references)
+
+
+
+
+// map.addLayer(references.category_miscellaneous.leafletgroup) // needs to be done based on user settings
+// map.removeLayer(references.category_satellite_dishes.leafletgroup) // needs to be done based on user settings
+
+
+
+
+
+// information_coords = document.getElementById('information_coords')
+// function updateCoords(e) {
+//     [x,y] = convertLeafletToGame([e.latlng.lat, e.latlng.lng])
+//     information_coords.innerHTML = `x: <u>${roundNumber(x, 2)}</u><br>y: <u>${roundNumber(y, 2)}</u>`
+// }
+// map.on('mousemove', updateCoords)
+// map.on('click', updateCoords)
+
