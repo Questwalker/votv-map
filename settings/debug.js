7d88d10960800c6c816489764dc4e0a217940368
QuestwalkerKO <94035647+Questwalker@users.noreply.github.com>
2025-04-02 03:20:13 -0600

    support for settings hot-editing

--- settings/debug.js
+++ settings/debug.js
@@ -1,24 +1,21 @@
 
 
 
-
-
-function accessInput(element, {edit=false, newvalue}) {
-    // Abstracts away different methods for reading/writing various input elements
-    switch (element.type) {
-        case 'checkbox':
-            if (edit) element.checked = newvalue
-            return element.checked
-        default:
-            console.log('error')
-            break
+function hotedit() {
+    let settings_id = this.dataset.id
+    let currentvalue = accessWidget(this, {})
+    if (currentvalue != settings.settings[settings_id]) {
+        settings.settings[settings_id] = currentvalue
+        let callback = settings.registered[settings_id].callback
+        if (typeof callback == 'function') {
+            callback(settings_id, currentvalue)
+        }
     }
+    pushToStorage()
+    syncWidgets()
 }
 
-
-
-
-
+dawf21fw12e1.addEventListener('click', hotedit)
 
 
 
