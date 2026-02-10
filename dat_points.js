15570516903364a58f399c8f7f1405b91f156852
QuestwalkerKO <94035647+Questwalker@users.noreply.github.com>
2026-02-10 09:23:42 +0000

    typo

--- dat_points.js
+++ dat_points.js
@@ -1288,7 +1288,7 @@ const markers = [
     },
     {
         name: "Car Battery Charger",
-        description: "Found on top of the workbench in the side room.<br><br>To use it, plug it into a nearby outlet, and then bring a car battery close (you can extract it from your ATV with a wrench).<br>When you bring a car battery close, the charger will automatically conenct to it and start charging it. Make sure the correct color clamps are connected to the correct color contacts, otherwise it'll spark and eventually explode. (Rotate the battery to change what contacts the battery charger connects to)",
+        description: "Found on top of the workbench in the side room.<br><br>To use it, plug it into a nearby outlet, and then grab a car battery (you can extract it from your ATV with a wrench).<br>When you bring a car battery close, the charger will automatically connect to it and start charging it. Make sure the correct color clamps are connected to the correct color contacts, otherwise it'll spark and eventually explode. (Rotate the battery to change what contacts the battery charger connects to)",
         related_images: [],
         category: "",
         icon: "./icons/carbatterycharger.png",
